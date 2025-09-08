c4f5b5ba9c69e7ba1ff121023a5eb0cc992bbc57
Harshith Teja <harshithtejadontoju@gmail.com>
2025-09-08 09:49:53 +0530

    built the Patient Controller; added endpoint to get all patients

---
+++ patient-service/src/main/java/com/ps/patientservice/controller/PatientController.java
@@ -0,0 +1,29 @@
+package com.ps.patientservice.controller;
+
+import com.ps.patientservice.dto.PatientResponseDTO;
+import com.ps.patientservice.service.PatientService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PatientResponseDTO>> getAllPatients() {
+        List<PatientResponseDTO> patients = patientService.getAllPatients();
+
+        return new ResponseEntity<>(patients, HttpStatus.OK);
+    }
+}
