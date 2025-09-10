21275c97e7cabc221de670a9e54104cdad54ee09
Harshith Teja <harshithtejadontoju@gmail.com>
2025-09-10 10:04:51 +0530

    added controller logic to delete a user

--- patient-service/src/main/java/com/ps/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/ps/patientservice/controller/PatientController.java
@@ -45,4 +45,11 @@ public class PatientController {
 
         return new ResponseEntity<>(patientResponseDTO, HttpStatus.OK);
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePatient(@PathVariable UUID id) {
+        patientService.deletePatient(id);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
