6ab73056d21a042f318b2d1ec6dc946e9b90cf54
Harshith Teja <harshithtejadontoju@gmail.com>
2025-09-21 16:45:56 +0530

    built authService to validate the pwd and generate jwt token

---
+++ auth-service/src/main/java/com/ps/authservice/Service/AuthService.java
@@ -0,0 +1,33 @@
+package com.ps.authservice.Service;
+
+import com.ps.authservice.dto.LoginRequestDTO;
+import com.ps.authservice.util.JwtUtil;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import com.ps.authservice.model.User;
+
+import java.util.Optional;
+
+@Service
+public class AuthService {
+
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    public AuthService(UserService userService, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
+    }
+
+    public Optional<String> authenticate(LoginRequestDTO loginRequestDTO) {
+
+        Optional<String> token = userService.findByEmail(loginRequestDTO.getEmail())
+                .filter(user -> passwordEncoder.matches(loginRequestDTO.getPassword(), user.getPassword()))
+                .map(user -> jwtUtil.generateToken(user.getEmail(), user.getRole()))
+                ;
+
+        return token;
+    }
+}
