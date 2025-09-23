82aaca93f0b9ac43ba780c3ab2e4acb6699274d2
Harshith Teja <harshithtejadontoju@gmail.com>
2025-09-23 18:12:43 +0530

    created AuthIntegrationTest class; added test for valid token request

---
+++ integration-tests/src/test/java/AuthIntegrationTest.java
@@ -0,0 +1,42 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class AuthIntegrationTest {
+
+    @BeforeAll
+    static void setup() {
+        RestAssured.baseURI = "http://localhost:4004";
+    }
+
+    @Test
+    public void shouldReturnOkWithValidToken() {
+        // 1.Arrange - set up the data that is needed for the test
+        // 2.Act - perform actions like triggering a flow, making a req to an endpoint
+        // 3.Assert - check if the actual response and expected response are same or not
+
+        String loginPayload = """
+                {
+                    "email": "testuser@test.com",
+                    "password": "password123"
+                }
+                """;
+
+        Response response = given() //Arrange
+                .contentType("application/json")
+                .body(loginPayload)
+                .when()             //Act
+                .post("/auth/login")
+                .then()             //Assert
+                .statusCode(200)
+                .body("token", notNullValue())
+                .extract()
+                .response();
+
+        System.out.println("Generated token: " + response.jsonPath().getString("token"));
+    }
+}
